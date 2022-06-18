91a46bc61933006537f95e3b7c3ddf524abd393d
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-06-18 19:32:07 +0100

    Adding publish date to the blog

--- I4G031012GZG/blog/models.py
+++ I4G031012GZG/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -13,6 +14,7 @@ class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    publish_date=models.DateTimeField(default=timezone.now)
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
