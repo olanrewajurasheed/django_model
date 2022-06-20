6f9a3eaa0af00a865553462ea75a535544fa4773
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-06-20 11:42:11 +0100

    final modification

--- I4G031012GZG/blog/models.py
+++ I4G031012GZG/blog/models.py
@@ -4,24 +4,15 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-
-STATUS = (
-    (0,"Draft"),
-    (1,"Publish")
-)
-
 class Post(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
-    publish_date=models.DateTimeField(default=timezone.now)
-    updated_on = models.DateTimeField(auto_now= True)
-    content = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    status = models.IntegerField(choices=STATUS, default=0)
+    Title = models.CharField(max_length=200, unique=True)
+    Text = models.TextField()
+    Author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    Created_date = models.DateTimeField(auto_now_add=True)
+    Published_date=models.DateTimeField(default=timezone.now)
 
     class Meta:
-        ordering = ['-created_on']
+        ordering = ['-Created_date']
 
     def __str__(self):
-        return self.title
+        return self.Title
