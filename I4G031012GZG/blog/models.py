9715a175ea8e35ba01bbeb192326575789fb8783
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-06-22 16:16:03 +0100

    User model correction

--- I4G031012GZG/blog/models.py
+++ I4G031012GZG/blog/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 
 class Post(models.Model):
     Title = models.CharField(max_length=200, unique=True)
     Text = models.TextField()
-    Author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    Author = models.ForeignKey(get_user_model(), on_delete= models.CASCADE,related_name='blog_posts')
     Created_date = models.DateTimeField(auto_now_add=True)
     Published_date=models.DateTimeField(default=timezone.now)
 
